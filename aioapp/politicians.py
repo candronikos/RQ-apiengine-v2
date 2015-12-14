f289e0fd6e5cd9733ba3459bc745daf40f3d8490
Christopher Andronikos <christopher.andronikos@gmail.com>
2015-12-14 14:07:39 +1100

    Import API class from generic server

--- aioapp/politicians.py
+++ aioapp/politicians.py
@@ -1,28 +1,28 @@
 #Working script using gremlinclient
 #https://github.com/davebshow/gremlinclient
 import asyncio
+from aiohttp import web
 from gremlinclient import AioGremlinClient
 
 class Politicians():
-    def __init__(self, url='ws://172.17.0.2:8182'):
+    def __init__(self, app, url='ws://172.17.0.2:8182'):
         self.client = AioGremlinClient(url=url)
 
-    async def handler(self, request=None):
-        resp = await self.client.submit("1 + 5")
+        app.router.add_route('GET', '/politicians/{item:\d+}', self.handler)
+
+        from tornado.platform.asyncio import AsyncIOMainLoop
+        AsyncIOMainLoop().install() # Use the asyncio event loop
+
+    async def handler(self, request):
+        d = request.match_info
+        resp = await self.client.submit("item * item",
+                bindings={'item':int(d['item'])})
+
+        body = None
         while True:
             msg = await resp.read()
             if msg is None:
                 break
-            #print(msg)
-            print("Result: %d" % msg.data[0])
+            body = msg.data[0]
 
-if __name__ == '__main__':
-    from tornado.platform.asyncio import AsyncIOMainLoop
-
-    AsyncIOMainLoop().install() # Use the asyncio event loop
-    loop = asyncio.get_event_loop()
-
-    app = Politicians()
-
-    loop.run_until_complete(app.handler())
-    app.client.close()
+        return web.Response(body=bytes(str(body), 'utf-8'))
---
+++ aioapp/rq.py
@@ -0,0 +1,33 @@
+import asyncio
+from aiohttp import web
+from politicians import Politicians
+
+app = web.Application()
+politicians = Politicians(app)
+
+loop = asyncio.get_event_loop()
+handler = app.make_handler()
+f = loop.create_server(handler, '0.0.0.0', 8080)
+srv = loop.run_until_complete(f)
+
+#Print serving address and port
+sn = srv.sockets[0].getsockname()
+print('Serving on:')
+print('  http://%s:%s\n' % (sn[0], sn[1]))
+
+#Display Routes
+print("Routing table:")
+for route in app.router.routes():
+    print("  " + str(route))
+
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+finally:
+    loop.run_until_complete(handler.finish_connections(1.0))
+    srv.close()
+    loop.run_until_complete(srv.wait_closed())
+    loop.run_until_complete(app.finish())
+
+loop.close()
